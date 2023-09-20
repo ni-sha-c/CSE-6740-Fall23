2b02b15f6b3d388077468dd709d09c5d856acb62
Nisha Chandramoorthy <nishac@mit.edu>
2023-09-20 19:41:30 -0400

    start with boosting question

---
+++ source/hw2_p1.py
@@ -0,0 +1,6 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+data = load_breast_cancer()
+X = data.data
+y = data.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
